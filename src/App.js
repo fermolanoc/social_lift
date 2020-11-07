1ff4ce112458dfeebbd61c44a1cbaa05971f16aa
Fernando Molano <fm87usa@gmail.com>
2020-11-07 11:23:29 -0600

    create footer, fix behavioir on main cards:hover event, improved fonts linking method

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,8 +18,30 @@ function Footer() {
                     <button type="submit">Send message</button>
                 </form>
             </div>
+            <div className="footer__content__sidemap">
+            <h2>Sitemap</h2>
+                <p>All our pages</p>
+                <div className="footer__content__sitemap--list">
+                  <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Contact</li>
+                  </ul>
+                </div>
+            </div>
+            <div className="footer__content__contact">
+            <div className="footer__company__name">Social Lift</div>
+            <p>Contact info</p>
+            <div className="footer__content__contact__details">
+              <div className="footer__content__contact--phone"><span className="fas fa-phone"></span> +1 612 098 7659</div>
+              <div className="footer__content__contact--email"><span className="fas fa-envelope"></span> email@sociallift.com</div>
+              <div className="footer__content__contact--address"><span className="fas fa-map-marker-alt"></span> 729 St SE, Office 101 <br></br>Dinkytown, Minneapolis, MN</div>
+            </div>
+            </div>
         </div>
-      <p className="copyright">Copyright ⓒ {year} | <b> Social Lift</b></p>
+      <p className="copyright">Copyright ⓒ {year} All Rights Reserved | <b> Social Lift</b></p>
     </footer>
   );
 }
