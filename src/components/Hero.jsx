349d8bdff36b9e781ea797cb665dc630fdd1568b
Fernando Molano <fm87usa@gmail.com>
2020-11-08 16:58:50 -0600

    add queries for responsive areas. Fix footer content on smaller divices, add -about us- section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ function Hero() {
         <div className="hero">
             <div className="hero__content">
                 <div className="hero__content--text">
-                    <h1>Hero Title Example</h1>
-                     <p>Hero Sub-title Description</p>
+                    <h1>A nice title about the product</h1>
+                     <p>Describe one or two features and why they are great</p>
                 </div>
                 <div className="hero__content--img">
                 </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,45 @@ import React from "react";
 function Main() {
   return (
     <main>
+      <div className="about-us">
+        <div className="about-us__container">
+          <div className="about-us__img">
+            <picture>
+              <img src="https://source.unsplash.com/random?workteam" alt=""/>
+            </picture>
+          </div>
+          <div className="about-us__content">
+            <div className="about-us__content--title">What we do...</div>
+            <div className="about-us__content--description">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+              unde tempora ad. Voluptas numquam veniam eius ducimus repellendus
+              earum distinctio, sit consequatur nostrum laboriosam soluta!
+              Similique itaque accusamus ut molestias.<br></br> Facere repellat quia
+              aliquam quisquam distinctio reprehenderit quae mollitia ab id
+              ullam magni fuga voluptatem tenetur tempore, amet suscipit odio?
+              Reprehenderit dolor tempore distinctio corporis deleniti magnam
+              suscipit ipsum quibusdam! Neque suscipit laborum rerum, nemo
+              adipisci dolore magni. Nostrum porro quibusdam debitis expedita
+              odio deleniti fugiat atque nihil sed nisi facilis veritatis fuga
+              incidunt aspernatur cumque sunt laboriosam, culpa illo! Odit
+              cumque aperiam, quis autem facere iusto eveniet voluptates
+              repellendus ad, inventore reprehenderit laboriosam optio, aliquam
+              similique eos aspernatur a id minima beatae quos. <br></br>Nostrum, nobis?
+              Enim numquam nisi cupiditate! Consectetur quisquam maiores
+              doloremque illum error deleniti reiciendis! Earum optio, amet sit
+              itaque velit ipsa laborum esse voluptatum culpa vero deserunt
+              accusamus illum non. Eos, consequatur consequuntur! Doloremque,
+              similique omnis.
+            </div>
+            <div><button className="about-us__content--button button">Read more...</button></div>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
       <section className="carousel__new">
         <div className="category__header">
           <h2 className="category__header--title">Best Rated Employees</h2>
-          <button className="category__header--button">Rate an employee</button>
+          <button className="category__header--button button">Rate an employee</button>
         </div>
         <div className="carousel__new__container">
           <div className="carousel__new--item">
